3ced7ef8405b48abfd83ffd6f97c7b86c6d3d4b7
mattlumsden229 <121324815+mattlumsden229@users.noreply.github.com>
2023-01-17 10:59:42 -0600

    added another person

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
     const jose = document.querySelector('#jose')
     const ethan = document.querySelector('#ethan')
     const nathan = document.querySelector('#nathan')
+    const niknik = document.querySelector('#niknik')
     
     const purple = document.querySelector('#purple')
     const black = document.querySelector('#black')
@@ -16,6 +17,7 @@ function check() {
     if(person === "matt"){
 	    matt.classList.toggle('hidden')
         jose.classList.add('hidden')
+        niknik.classList.add('hidden')
         ethan.classList.add('hidden')
         brown.classList.add('hidden')
         black.classList.add('hidden')
@@ -24,6 +26,7 @@ function check() {
     }else if(person === "jose"){
         jose.classList.toggle('hidden')
         ethan.classList.add('hidden')
+        niknik.classList.add('hidden')
         matt.classList.add('hidden')
         purple.classList.add('hidden')
         brown.classList.add('hidden')
@@ -31,6 +34,7 @@ function check() {
         document.querySelector("#findOut").innerText = "Jose is a two stripe black belt and is the head coach of Kato Jiu-Jitsu!"
     }else if(person === "ethan"){
         ethan.classList.toggle('hidden')
+        niknik.classList.add('hidden')
         matt.classList.add('hidden')
         jose.classList.add('hidden')
         purple.classList.add('hidden')
@@ -40,11 +44,21 @@ function check() {
 }else if(person === "nathan"){
         nathan.classList.toggle('hidden')
         jose.classList.add('hidden')
+        niknik.classList.add('hidden')
         matt.classList.add('hidden')
         ethan.classList.add('hidden')
         brown.classList.add('hidden')
         black.classList.add('hidden')
         purple.classList.toggle('hidden')
-        document.querySelector("#findOut").innerText = "Matt recieved his purple belt on 11/19/2022 at Kato Jiu-Jitsu from Jose Carlos Lima after almost 3 years of hard work at blue belt!"
+        document.querySelector("#findOut").innerText = "Nathan recieved his purple belt on 11/19/2022 at Kato Jiu-Jitsu from Jose Carlos Lima."
+}else if(person === "nik nik"){
+    niknik.classList.toggle('hidden')
+    jose.classList.add('hidden')
+    ethan.classList.add('hidden')
+    matt.classList.add('hidden')
+    purple.classList.add('hidden')
+    brown.classList.add('hidden')
+    black.classList.toggle('hidden')
+    document.querySelector("#findOut").innerText = "Nik Nik is a black belt under Jose Carlos Lima. Here's a picture of him recieving his black belt certificate."
 }
 }
